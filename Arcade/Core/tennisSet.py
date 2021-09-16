a394fe63421a7de9450ec04adeac569fd4828cd7
Sreedevi Varma <23123024+sree-varma@users.noreply.github.com>
2021-09-16 11:44:57 +0530

    Create tennisSet.py

---
+++ Arcade/Core/tennisSet.py
@@ -0,0 +1,13 @@
+"""
+In tennis, the winner of a set is based on how many games each player wins. The first player to win 6 games is declared the winner unless their opponent had already won 5 games, in which case the set continues until one of the players has won 7 games.
+
+Given two integers score1 and score2, your task is to determine if it is possible for a tennis set to be finished with a final score of score1 : score2.
+"""
+def tennisSet(score1, score2):
+    if score1==6 or score2==6:
+        if score1<5 or score2<5:
+            return True
+    if score1==7 or score2==7:
+        if 5<=score1<7 or 5<=score2<7:
+            return True
+    return False
